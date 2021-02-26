e4a074b61032d4e01e609eaf01387d09aa46823f
makssen <maks.sen.1903@mail.ru>
2021-02-27 00:22:02 +0300

    fead: make towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || !matrix.length) return [];
+    let res = [];
+    matrix.forEach((item, i) => {
+        if (i % 2 === 1) {
+            item.reverse();
+        }
+        item.forEach(elem => {
+            res.push(elem);
+        });
+    });
+    return res;
 }
